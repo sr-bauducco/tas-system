ccb85b827b72f7393c393e9c2616dd1af851047c
sr-bauducco <israel.telesbandeira@gmail.com>
2026-04-08 19:47:39 -0300

    g10, fully implemented as so

---
+++ ms-emergency/src/main/java/EmergencyApplication.java
@@ -0,0 +1,10 @@
+package tas.services.emergency;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmergencyApplication.class, args);
+    }
+}
