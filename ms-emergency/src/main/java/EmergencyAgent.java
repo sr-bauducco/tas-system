e51d3c590f4979cafccf9d204cb3b1d925624b47
sr-bauducco <israel.telesbandeira@gmail.com>
2026-04-10 10:54:21 -0300

    merged sucessfuly

--- ms-emergency/src/main/java/EmergencyAgent.java
+++ ms-emergency/src/main/java/EmergencyAgent.java
@@ -1,5 +1,4 @@
 package tas.services.emergency;
-<<<<<<< HEAD
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -70,20 +69,3 @@ public class EmergencyAgent implements G10NotifyEmergency {
         .timeout(Duration.ofSeconds(3)); 
     }
 }
-=======
-import org.springframework.stereotype.Service;
-import tas.goals.G10NotifyEmergency;
-import reactor.core.publisher.Mono;
-import java.time.Duration;
-
-@Service
-public class EmergencyAgent implements G10NotifyEmergency {
-    @Override
-    public Mono<FulfillmentStatus> execute(EmergencyContext context) {
-        // Implementation of G10 logic with 2s timeout (QoS)
-        return Mono.just(new FulfillmentStatus("SUCCESS", "REST_API"))
-                   .delayElement(Duration.ofMillis(100))
-                   .timeout(Duration.ofSeconds(2));
-    }
-}
->>>>>>> main
