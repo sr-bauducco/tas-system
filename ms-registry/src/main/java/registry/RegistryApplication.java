8367d6ef36fc9a26bd185c8a25ceb3dd0d48e427
sr-bauducco <israel.telesbandeira@gmail.com>
2026-05-04 17:06:34 -0300

    ms-registry working

---
+++ ms-registry/src/main/java/registry/RegistryApplication.java
@@ -0,0 +1,13 @@
+package registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // This is the crucial annotation
+public class RegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
